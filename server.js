add05fc6b6e157dd70d6d6f8f3b2e538599b38cf
stephanie <stephanie.assifoua@student.heaj.be>
2020-04-29 12:43:40 +0200

    refix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const index = '/pictionary.html';
 
 const server = express()
     .use((req, res) => res.sendFile(index, { root: __dirname }))
-    .listen(port, () => console.log('Listening on port', port));
+    .listen(port, () => console.log('Listening on port ', port));
 
 const io = socketIO(server);
 
@@ -19,12 +19,24 @@ io.on('connection', (socket) => {
 });
 
 let users = [];
+let currentPlayer = null;
+let timeout;
+let words = ['apple', 'windows', 'linux'];
 
 function onConnection(socket) {
     socket.on('username', (username) => {
         console.log('Client name: ', username);
         socket.username = username;
-        users.push(socket);
+
+        if (users.length === 0) {
+            currentPlayer = socket;
+            timeout = clearTimeout(timeout);
+            users.push(socket);
+            switchPlayer();
+        } else {
+            users.push(socket);
+        }
+
         sendUsers();
     });
 
@@ -37,11 +49,31 @@ function onConnection(socket) {
             return user !== socket;
         });
         sendUsers();
+        if (users.length === 0) {
+            timeout = clearTimeout(timeout);
+        }
     });
 }
 
 function sendUsers() {
     io.emit('users', users.map((user) => {
-        return user.username;
-    }));
+        return {
+            username: user.username,
+            active: user === currentPlayer
+        };
+    }))
+}
+
+function switchPlayer() {
+    if (users.length === 0) return;
+
+    const indexCurrentPlayer = users.indexOf(currentPlayer);
+    console.log(indexCurrentPlayer, (indexCurrentPlayer + 1) % users.length);
+    currentPlayer = users[(indexCurrentPlayer + 1) % users.length];
+
+    sendUsers();
+    timeout = setTimeout(switchPlayer, 20000);
+
+    currentPlayer.emit('word', words[Math.floor(words.length * Math.random())]);
+    io.emit('clear');
 }
